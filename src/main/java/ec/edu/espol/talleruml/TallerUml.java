69ed9715efbe7b5508ab0bfea86b1bc9cf747fed
AlexanderSorianoLeon <127567902+JulissaMC@users.noreply.github.com>
2023-11-08 15:25:56 -0500

    first commit

---
+++ src/main/java/ec/edu/espol/talleruml/TallerUml.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.talleruml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class TallerUml {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
